20eeb40d038418ccabe0bff31990d811755bc925
Robbie Corcoran <codewithrobbie@gmail.com>
2023-10-25 12:39:12 +0200

    Test Create User - User Details JSON

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,6 +1,10 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -10,16 +14,27 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 //        properties = "server.port=8081")
 public class UsersControllerIntegrationTest {
 
-//    @Value("${server.port}")
-//    private int serverPort;
+    @Value("${server.port}")
+    private int serverPort;
 
     @LocalServerPort
     private int localSeverPort;
 
     @Test
-    void test() {
-//        System.out.println("Server.port= " + serverPort);
-        System.out.println("Local server port= " + localSeverPort);
+    @DisplayName("User can be created.")
+    void testCreateUser_whenValidDetailsProvided_returnUserDetails() throws JSONException {
+//        Arrange
+        JSONObject userDetailsRequestJson = new JSONObject();
+        userDetailsRequestJson.put("firstName", "Robbie");
+        userDetailsRequestJson.put("lastName", "Corcoran");
+        userDetailsRequestJson.put("email", "robbie@robbie.com");
+        userDetailsRequestJson.put("password", "123456789");
+        userDetailsRequestJson.put("repeatPassword", "123456789");
+
+
+//        Act
+
+//        Assert
     }
 
 }
