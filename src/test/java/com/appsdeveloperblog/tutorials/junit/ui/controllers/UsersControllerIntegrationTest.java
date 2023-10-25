77f403543193636ec629989f43977370edf326c1
Robbie Corcoran <codewithrobbie@gmail.com>
2023-10-25 12:04:43 +0200

    @TestPropertySource. Loading alternative configuration.

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,14 +1,21 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = "server.port=8081")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource(
+        locations = "/application-test.properties",
+        properties = "server.port=8081")
 public class UsersControllerIntegrationTest {
 
+    @Value("${server.port}")
+    private int serverPort;
     @Test
     void test() {
-
+        System.out.println("Server.port= " + serverPort);
     }
 
 }
