e71cc3a83557dbf3483395f89bc72d6b7bb6e3d1
Robbie Corcoran <codewithrobbie@gmail.com>
2023-10-25 12:51:39 +0200

    TestRestTemplate - Prepare & Perform HTTP Post Request

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,12 +1,19 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@TestPropertySource(
@@ -14,11 +21,8 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 //        properties = "server.port=8081")
 public class UsersControllerIntegrationTest {
 
-    @Value("${server.port}")
-    private int serverPort;
-
-    @LocalServerPort
-    private int localSeverPort;
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
     @Test
     @DisplayName("User can be created.")
@@ -31,10 +35,31 @@ public class UsersControllerIntegrationTest {
         userDetailsRequestJson.put("password", "123456789");
         userDetailsRequestJson.put("repeatPassword", "123456789");
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> request = new HttpEntity<>(userDetailsRequestJson.toString(), headers);
 
 //        Act
+        ResponseEntity<UserRest> createdUserDetailsEntity = testRestTemplate.postForEntity(
+                "/users",
+                request,
+                UserRest.class);
+        UserRest createdUserDetails = createdUserDetailsEntity.getBody();
 
 //        Assert
+        assertEquals(
+                HttpStatus.OK,
+                createdUserDetailsEntity.getStatusCode(),
+                "createUser did should have returned a 200 status code.");
+        assertEquals(userDetailsRequestJson.getString(
+                        "firstName"),
+                createdUserDetails.getFirstName(),
+                "The User's first name is not correct.");
+        assertFalse(
+                createdUserDetails.getUserId().trim().isEmpty(),
+                "User's ID should not be empty.");
     }
 
 }
