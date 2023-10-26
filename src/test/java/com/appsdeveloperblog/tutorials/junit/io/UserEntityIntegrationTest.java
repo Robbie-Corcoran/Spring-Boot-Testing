3718fb06082c9f9b3c256d68f9b95d5d1290064a
Robbie Corcoran <codewithrobbie@gmail.com>
2023-10-26 15:29:38 +0200

    Setup UserEntityIntegrationTest.java & added @DataJpaTest.

---
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/io/UserEntityIntegrationTest.java
@@ -0,0 +1,7 @@
+package com.appsdeveloperblog.tutorials.junit.io;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class UserEntityIntegrationTest {
+}
